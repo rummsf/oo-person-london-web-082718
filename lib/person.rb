0e489d07dd7a6b099e6056e525b913a2f8e63856
Irum <irum@Irums-MacBook-Air.local>
2018-09-19 16:11:11 +0100

    Done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,1 +1,68 @@
-# your code goes here
+class Person
+
+  attr_accessor :bank_account, :happiness, :hygiene
+  attr_reader :name
+
+  def initialize(name)
+    @name = name
+    @happiness = 8
+    @bank_account = 25
+    @hygiene = 8
+  end
+
+  def happiness=(level)
+      @happiness = level
+      @happiness = 10 if @happiness > 10
+      @happiness = 0 if @happiness < 0
+  end
+
+  def hygiene=(level)
+      @hygiene = level
+      @hygiene = 10 if @hygiene > 10
+      @hygiene = 0 if @hygiene < 0
+  end
+
+  def clean?
+    hygiene > 7
+  end
+
+  def happy?
+    happiness > 7
+  end
+
+  def get_paid(income)
+    self.bank_account += income
+     "all about the benjamins"
+  end
+
+  def take_bath
+    self.hygiene += 4
+    "♪ Rub-a-dub just relaxing in the tub ♫"
+  end
+
+  def work_out
+    self.happiness += 2
+    self.hygiene -= 3
+    "♪ another one bites the dust ♫"
+  end
+
+  def call_friend(friend)
+    self.happiness += 3
+    friend.happiness += 3
+    "Hi #{friend.name}! It's #{self.name}. How are you?"
+  end
+
+  def start_conversation(person, topic)
+    if topic == "politics"
+      person.happiness -= 2
+      self.happiness -= 2
+        "blah blah partisan blah lobbyist"
+    elsif topic == "weather"
+      person.happiness += 1
+      self.happiness += 1
+      "blah blah sun blah rain"
+    else "blah blah blah blah blah"
+    end
+  end
+
+end
